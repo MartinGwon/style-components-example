c22d9ab1b9828eb9073e2282b652033795870d62
MartinGwon <anatomy1545@gmail.com>
2020-03-01 03:43:19 +0900

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding:0;
+    margin:0;
+  }
+`;
+
+class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <GlobalStyle />
+        <Button>Accept</Button>
+        <Button danger>Deny</Button>
+        <Anchor as="a" href="http://www.google.com">
+          Back to Google
+        </Anchor>
+      </Container>
+    );
+  }
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 100px;
+  color: white;
+  font-weight: 400;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
+
+const Anchor = styled(Button)`
+  text-decoration: none;
+`;
 
 export default App;
